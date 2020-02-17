884adc87104bdd65b5800e76949154c34d0873b7
Josema <enzo@josema.es>
2020-02-17 16:27:36 +0100

    First commit

---
+++ src/components/App.svelte
@@ -0,0 +1,23 @@
+<script>
+    import Button from './Button.svelte'
+</script>
+
+<style>
+    @font-face {
+        font-family: 'Supercell-Magic';
+        font-style: normal;
+        font-weight: 400;
+        src: local('Supercell-Magic'), local('Supercell-Magic'),
+            url(/assets/fonts/Supercell-Magic.ttf) format('ttf');
+    }
+
+    * {
+        font-family: Supercell-Magic;
+    }
+</style>
+
+<main>
+    <h1>Hello CodeSandbox</h1>
+    <h2>Start editing to see some magic happenssssss nooo!</h2>
+    <Button />
+</main>
